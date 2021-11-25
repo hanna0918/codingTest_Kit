7ab706189b6ef6ec4064cdd9525175976fa0378f
hanna <kkgjt@naver.com>
2021-11-25 14:41:42 +0900

    20211125 XintervalNum

---
+++ src/XintervalNum.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class XintervalNum {
+    public long[] solution(long x, int n) {
+        long[] answer = new long[n];
+        
+        for(int i=0; i<n; i++){
+            answer[i] = (i+1)*x;
+        }
+        return answer;
+    }
+}
