d877f636d55447112cb2a9c01e1dbae96c7b826d
hanna <kkgjt@naver.com>
2021-12-19 00:00:50 +0900

    2021.12.19 정수 내림차순 정렬

--- src/NumberDesc.java
+++ src/NumberDesc.java
@@ -2,7 +2,8 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class NumberDesc {
-
+	//n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴
+	//n이 118372면 873211을 리턴
 
 	class Solution {
 		public long solution(long n) {
