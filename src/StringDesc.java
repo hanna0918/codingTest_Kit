8cf06b01e345db1424267af00d146698765d3387
hanna <kkgjt@naver.com>
2021-12-22 21:25:37 +0900

    2021.12.22 StringDESC

--- src/StringDesc.java
+++ src/StringDesc.java
@@ -1,7 +1,8 @@
 import java.util.*;
 
 public class StringDesc {
-
+	//문자열 s에 나타나는 문자를 큰것부터 작은 순으로 정렬해 새로운 문자열을 리턴하는 함수
+	//s는 영문 대소문자로만 구성되어 있으며, 대문자는 소문자보다 작은 것으로 간주
 
 	class Solution {
 	    public String solution(String s) {
