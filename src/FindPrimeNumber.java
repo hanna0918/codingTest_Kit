2a21ccacb5da9c551cd7f7b1326e6a5b16d11e8a
hanna <kkgjt@naver.com>
2022-01-25 22:38:43 +0900

    2022.01.25 Find PrimeNumber

---
+++ src/FindPrimeNumber.java
@@ -0,0 +1,28 @@
+
+public class FindPrimeNumber {
+	public int solution(int n){
+        int answer = 0;
+        int[] arr = new int[n+1];
+        
+        for(int i=2; i<=n; i++){
+            arr[i] = i;
+        }
+        
+        for(int i=2; i*i<=n; i++){
+            if(arr[i]==0){
+                continue;
+            }
+            for(int j=2*i; j<=n; j=j+i){
+                arr[j] = 0;
+            }
+        }
+        
+        for(int i=2; i<=n; i++){
+            if(arr[i]!=0){
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
